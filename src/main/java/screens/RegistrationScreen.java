32e80ace570151b41b3d77bd004cf78dc999444a
tdshe <tnshvts1602@gmail.com>
2025-01-23 13:01:46 +0200

    HOMEWORK 22

--- src/main/java/screens/RegistrationScreen.java
+++ src/main/java/screens/RegistrationScreen.java
@@ -1,5 +1,6 @@
 package screens;
 
+import dto.UserDTO;
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.android.AndroidElement;
 import org.openqa.selenium.support.FindBy;
@@ -23,4 +24,39 @@ public class RegistrationScreen extends BaseScreen
 
     @FindBy(id = "com.telran.ilcarro:id/editRegPassword")
     AndroidElement inputPassword;
+
+    @FindBy(id = "com.telran.ilcarro:id/checkBoxAgree")
+    AndroidElement checkbox;
+
+    @FindBy(id = "com.telran.ilcarro:id/regBtn")
+    AndroidElement btnYalla;
+
+    @FindBy(xpath = "//*[@text='Registration success!']")
+    AndroidElement popUpMessage;
+
+    @FindBy(id = "android:id/message")
+    AndroidElement errorMessage;
+
+    @FindBy(xpath = "android:id/button1")
+    AndroidElement btnOkErrorMessage;
+
+    public void typeRegistrationForm(UserDTO user)
+    {
+        inputName.sendKeys(user.getFirstName());
+        inputLastName.sendKeys(user.getLastName());
+        inputEmail.sendKeys(user.getUsername());
+        inputPassword.sendKeys(user.getPassword());
+        clickWait(checkbox,5);
+        clickWait(btnYalla,5);
+    }
+
+    public boolean popUpMessageIsExisted(String text)
+    {
+       return textElementIsPresent(popUpMessage, text, 5);
+    }
+
+    public boolean errorMessageIsExisted(String text)
+    {
+       return textElementIsPresent(errorMessage, text, 5);
+    }
 }
--- src/test/java/ui_mobile/RegistrationTests.java
+++ src/test/java/ui_mobile/RegistrationTests.java
@@ -1,10 +1,14 @@
 package ui_mobile;
 
 import config.AppiumConfig;
+import dto.UserDTO;
+import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+import screens.RegistrationScreen;
 import screens.SearchScreen;
 import screens.SplashScreen;
+import static helper.RandomHelper.*;
 
 public class RegistrationTests extends AppiumConfig
 {
@@ -15,10 +19,73 @@ public class RegistrationTests extends AppiumConfig
         new SearchScreen(driver).goToRegistrationPage();
     }
 
-
     @Test
     public void registrationPositiveTest()
     {
+        UserDTO user = UserDTO.builder()
+                .firstName(generateString(6))
+                .lastName(generateString(9))
+                .username(generateEmail(7))
+                .password("Password123!")
+                .build();
 
+        new RegistrationScreen(driver).typeRegistrationForm(user);
+        Assert.assertTrue(new RegistrationScreen(driver).popUpMessageIsExisted("Registration success!"));
+    }
+
+    @Test
+    public void registrationNegativeTest_EmptyName()
+    {
+        UserDTO user = UserDTO.builder()
+                .firstName("")
+                .lastName(generateString(9))
+                .username(generateEmail(7))
+                .password("Password123!")
+                .build();
+
+        new RegistrationScreen(driver).typeRegistrationForm(user);
+        Assert.assertTrue(new RegistrationScreen(driver).errorMessageIsExisted("All fields must be filled and agree terms"));
+    }
+
+    @Test
+    public void registrationNegativeTest_EmptyLastName()
+    {
+        UserDTO user = UserDTO.builder()
+                .firstName(generateString(6))
+                .lastName("")
+                .username(generateEmail(7))
+                .password("Password123!")
+                .build();
+
+        new RegistrationScreen(driver).typeRegistrationForm(user);
+        Assert.assertTrue(new RegistrationScreen(driver).errorMessageIsExisted("All fields must be filled and agree terms"));
+    }
+
+    @Test
+    public void registrationNegativeTest_WrongEmail()
+    {
+        UserDTO user = UserDTO.builder()
+                .firstName(generateString(6))
+                .lastName(generateString(9))
+                .username(generateString(10))
+                .password("Password123!")
+                .build();
+
+        new RegistrationScreen(driver).typeRegistrationForm(user);
+        Assert.assertTrue(new RegistrationScreen(driver).errorMessageIsExisted("{username=must be a well-formed email address}"));
+    }
+
+    @Test
+    public void registrationNegativeTest_WrongPassword()
+    {
+        UserDTO user = UserDTO.builder()
+                .firstName(generateString(6))
+                .lastName(generateString(9))
+                .username(generateEmail(7))
+                .password(generateNumbers(4))
+                .build();
+
+        new RegistrationScreen(driver).typeRegistrationForm(user);
+        Assert.assertTrue(new RegistrationScreen(driver).errorMessageIsExisted("{password= At least 8 characters; Must contain at least 1 uppercase letter, 1 lowercase letter, and 1 number; Can contain special characters [@$#^&*!]}"));
     }
 }
